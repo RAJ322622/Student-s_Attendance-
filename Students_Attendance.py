e721a1d319978c99a3ea9b570a5a8373d7054ce2
RAJ KUMAR A <146355426+RAJ322622@users.noreply.github.com>
2025-04-20 17:45:06 +0530

    Update Students_Attendance.py

--- Students_Attendance.py
+++ Students_Attendance.py
@@ -9,6 +9,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from streamlit.components.v1 import html
+import time
 
 # Initialize session state for authentication status
 if 'auth_status' not in st.session_state:
